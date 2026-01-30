7e2df600b4cb87b4de315137ef10ccca323dd397
shekofaalizada-ship-it <shekofa.alizada@auw.edu.bd>
2026-01-30 17:24:15 +0600

    Add files via upload

---
+++ cond1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num > 0)
+    {
+        printf("Positive\n");
+    }
+    else if (num < 0)
+    {
+        printf("Negative\n");
+    }
+    else
+    {
+        printf("Zero\n");
+    }
+
+    return 0;
+}
