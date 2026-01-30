3e91dfa3d7173fafd26ec6c1d3c368133b636132
shekofaalizada-ship-it <shekofa.alizada@auw.edu.bd>
2026-01-30 17:36:55 +0600

    Add files via upload

---
+++ loop18.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, j, num = 1;
+
+    for (i = 1; i <= 4; i++)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            printf("%d ", num);
+            num = num + 1;
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
